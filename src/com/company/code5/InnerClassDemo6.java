50e10ec4eedef6fbf6856ec12fcf758351bf8580
liting <757360513@qq.com>
2017-09-26 10:27:57 +0800

    init commit

--- src/com/company/code5/InnerClassDemo6.java
+++ src/com/company/code5/InnerClassDemo6.java
@@ -1,118 +1,39 @@
 package com.company.code5;
 /*
-	匿名内部类
-		1. 就是内部类的简化写法。
-        2. 定义匿名内部类的前提
-		    内部类必须是继承一个类或者实现接口
-	前提：存在一个抽象类或者接口
-
-	    3. 格式：
-		    new 类名或者接口名(){
-			  重写方法;
-	      	}
-		4. 其实匿名内部类就是一个匿名子类对象。而且这个对象有点胖。
-		5. 匿名内部类中定义的方法最好不要超过5个。
-	 小结：本质是什么呢?
-		是一个继承了该类或者实现了该接口的子类匿名对象。
-
+	匿名内部类思考题：
+		按照要求，补齐代码
+			interface Inter { void show(); }
+			class Outer { //补齐代码 }
+			class OuterDemo {
+				public static void main(String[] args) {
+					  Outer.method().show();
+				  }
+			}
+			要求在控制台输出”HelloWorld”
 */
-//可以定义接口
-abstract class AbsDemo {
-    public abstract void show();
-    public abstract void show2();
+interface Inter {
+    void show();
+    //public abstract
 }
 
 class Outer {
-    private int x =3;
-
-    //Step 1: 继承
-
-     class Inner extends AbsDemo
-     {
-     public void show()
-     {
-     System.out.println(" method : " + x);
-     }
-     public void show2()
-     {
-     System.out.println(" method : " + (x+1));
-     }
-
-
-     }
-
-
-
-
-
-    public void function() {
-        // Step 1
-        //new Inner().show();
-
-        // Step 2
-
-        //new AbsDemo();//错误
-        //new AbsDemo()
-        //{
-
-        //}.show();
-   /*    new AbsDemo(){
-           //带了对象方法体的 new 对象， 是AbsDemo子类对象，只有子类才能重写 show
-             public void show() {
-             System.out.println("show ：" + x);
-             }
-
-           @Override
-           public void show2() {
-
-           }
-
-       }.show();*/
-
-        //二个方法的时候
-		new AbsDemo() {
-			public void show() {
-				System.out.println("show");
-			}
-
-			public void show2() {
-				System.out.println("show2");
-			}
-		}.show();
-
-		new AbsDemo() {
-			public void show() {
-				System.out.println("show");
-			}
-
-			public void show2() {
-				System.out.println("show : 2");
-			}
-		}.show2();
-
-        //如果我是很多个方法，就很麻烦了
-        //那么，我们有没有改进的方案呢?
-        //Step 3
-
-        AbsDemo i = new AbsDemo() { //多态
+    //补齐代码
+    public static Inter method() {
+        //子类对象 -- 子类匿名对象
+        return new Inter() {
             public void show() {
-                System.out.println("show");
-            }
-
-            public void show2() {
-                System.out.println("show : 2");
+                System.out.println("HelloWorld");
             }
         };
-
-        i.show();
-        i.show2();
-
-
     }
 }
 public class InnerClassDemo6 {
     public static void main(String[] args) {
-        new Outer().function();
-
+        Outer.method().show();
+		/*
+			1:Outer.method()可以看出method()应该是Outer中的一个静态方法。
+			2:Outer.method().show()可以看出method()方法的返回值是一个对象。
+				又由于接口Inter中有一个show()方法,所以我认为method()方法的返回值类型是一个接口。
+		*/
     }
 }
