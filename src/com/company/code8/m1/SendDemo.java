5ae680ee8a17e8b72cf8e9929a7d978be9543568
liting <757360513@qq.com>
2017-09-30 17:09:19 +0800

    init commit

--- src/com/company/code8/m1/SendDemo.java
+++ src/com/company/code8/m1/SendDemo.java
@@ -1,4 +1,4 @@
-package cn.itcast_04;
+package com.company.code8.m1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,24 +13,26 @@ import java.net.InetAddress;
  */
 public class SendDemo {
 	public static void main(String[] args) throws IOException {
-
+		//新建UDP服务
 		DatagramSocket ds = new DatagramSocket();
 
-
+		//键入的数据存入buffer
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = null;
+		//读取文件
 		while ((line = br.readLine()) != null) {
+			//键入886则中断循环
 			if ("886".equals(line)) {
 				break;
 			}
 
-
+			//获取键入数据的字节信息
 			byte[] bys = line.getBytes();
-
+			//封装UDP报文
 			DatagramPacket dp = new DatagramPacket(bys, bys.length,
 					InetAddress.getByName("localhost"), 12345);
 
-
+			//发送报文
 			ds.send(dp);
 		}
 
